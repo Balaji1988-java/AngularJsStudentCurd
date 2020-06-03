07cba585eddfc03c53c61a6bfb74c3c9a5b5ea57
Balaji1988-java <balaji.shelgunte@gmail.com>
2020-06-03 11:43:45 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ { path: '', redirectTo: 'student', pathMatch: 'full' },
+{ path: 'students', component: StudentListComponent },
+{ path: 'add', component: CreateStudentComponent },
+{ path: 'update/:id', component: UpdateStudentComponent },
+{ path: 'details/:id', component: StudentDetailsComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateStudentComponent,
+    StudentDetailsComponent,
+    StudentListComponent,
+    UpdateStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
